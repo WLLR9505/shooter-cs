0eddcccf1591c154f457edc8e540d7d33cf24fc8
Weuller Jr. Souza <weuller9505@live.com>
2018-06-29 23:18:23 -0300

    Rotação

--- js/armas.js
+++ js/armas.js
@@ -1,6 +1,20 @@
+var angulo = 0,
+    rotacao = 0,
+    x = 300,
+    y = 150;
 var armaTeste = new Image();
 armaTeste.src = './resources/armas/pistola_1.png';
 
-function drawArma(CONTEXT) {
-    CONTEXT.drawImage(armaTeste, 300, 150);
+function drawArma (CONTEXT) {
+
+    angulo = rotacao * Math.PI / 180;
+    CONTEXT.save(); //para as alterações não afetarem nada até o restore()
+
+    CONTEXT.translate(x, y);
+    CONTEXT.rotate(angulo);
+
+    CONTEXT.drawImage(armaTeste, -armaTeste.width / 2, -armaTeste.height / 2);
+
+    CONTEXT.restore();
+    rotacao += 10;
 }
