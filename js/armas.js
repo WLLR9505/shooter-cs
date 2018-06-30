3ef17fbf2605117b56308eba76eabbcaa88e444d
Weuller Jr. Souza <weuller9505@live.com>
2018-06-30 00:11:54 -0300

    Mira com mouse

--- js/armas.js
+++ js/armas.js
@@ -7,14 +7,17 @@ armaTeste.src = './resources/armas/pistola_1.png';
 
 function drawArma (CONTEXT) {
 
-    angulo = rotacao * Math.PI / 180;
+    // angulo = rotacao * Math.PI / 180;
+    angulo = Math.atan2(mouseY - y, mouseX - x);
     CONTEXT.save(); //para as alterações não afetarem nada até o restore()
 
     CONTEXT.translate(x, y);
     CONTEXT.rotate(angulo);
 
+    if (mouseY >= y && mouseX <= x || mouseY <= y && mouseX <= x) {
+        CONTEXT.scale(1, -1);   //caso o mouse esteja a esquerda da arma
+    }
     CONTEXT.drawImage(armaTeste, -armaTeste.width / 2, -armaTeste.height / 2);
 
     CONTEXT.restore();
-    rotacao += 10;
 }
