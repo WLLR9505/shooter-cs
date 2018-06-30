519766b8a9aeb28a464ec63f6bb9836185c82ac2
Weuller Jr. Souza <weuller9505@live.com>
2018-06-30 20:07:57 -0300

    Miras equipáveis

--- js/armas.js
+++ js/armas.js
@@ -1,33 +1,75 @@
-class Weapon {
-    constructor (nome, alcance, velocidadeTiro, imgNome) {
+const at_MIRA = 0;
+const at_PENTE = 1;
+const at_CANO = 2;
+const at_ESPECIAL = 3;
+
+class Attachment {
+    constructor (nome, tipo, imgNome, extra) {
+        this.tipo = tipo;
         this.nome = nome;
-        this.alcance = alcance;
-        this.velocidadeTiro = velocidadeTiro;   // 2 ~ 5
-        this.img = new Image();
-        this.img.src = './resources/armas/' + imgNome;
+        this.extra = extra;
+        if (imgNome != undefined) {
+            this.img = new Image();
+            this.img.src = './resources/' + imgNome;
+        }
     }
 }
 
 class Sight {
-    constructor(nome, imgNome) {
+    constructor (nome, imgNome) {
         this.nome = nome;
         this.img = new Image();
-        this.img.src = './resources/armas/' + imgNome;
+        this.img.src = './resources/armas/attachments/' + imgNome;
     }
 }
 
-var pistola = new Weapon('P1', 30, 3, 'pistola_1.png');
-var fuzil1 = new Weapon('FZ-1A', 60, 4, 'fuzil_1.png');
-var shotgunCurta = new Weapon('SHT-C', 50, 2.5, 'shotgun_curta.png');
-var shotgunLonga = new Weapon('SHT-L', 55, 2.9, 'shotgun_longa.png');
+var miraComum = new Sight('Mira Comum', 'mira_comum.png');
+var atc_miraComum = new Attachment('Mira Comum', at_MIRA, undefined, miraComum);
+var miraPontoVermelho = new Sight('Ponto Vermelho', 'mira_ponto_vermelho.png');
+var atc_miraPontoVermelho = new Attachment('Ponto Vermelho', at_MIRA, undefined, miraPontoVermelho);
+var miraTatica = new Sight('Mira Tatica', 'mira_tatica.png');
+var atc_miraTatica = new Attachment('Mira Tatica', at_MIRA, undefined, miraTatica);
 
+class Weapon {
+    constructor (nome, alcance, velocidadeTiro, nAttachment, imgNome) {
+        this.nome = nome;
+        this.alcance = alcance;
+        this.velocidadeTiro = velocidadeTiro;   // 2 ~ 5
+        this.attachment = new Array(nAttachment);
+        this.attachment[0] = atc_miraComum;
+        this.img = new Image();
+        this.img.src = './resources/armas/' + imgNome;
+    }
+    ConnectAttachment (attachment) {
+        switch (attachment.tipo) {
+            case at_MIRA:
+                this.attachment[0] = attachment;
+                break;
+            case at_PENTE:
+                this.attachment[1] = attachment;
+                break;
+            case at_CANO:
+                this.attachment[2] = attachment;
+                break;
+            case at_ESPECIAL:
+                this.attachment[3] = attachment;
+                break;
+        }
+    }
+}
+
+var pistola = new Weapon('P1', 30, 3, 2, 'pistola_1.png');
+var fuzil1 = new Weapon('FZ-1A', 60, 4, 3,'fuzil_1.png');
+var shotgunCurta = new Weapon('SHT-C', 50, 2.5, 1, 'shotgun_curta.png');
+var shotgunLonga = new Weapon('SHT-L', 55, 2.9, 1, 'shotgun_longa.png');
+
+
+fuzil1.ConnectAttachment(atc_miraTatica);
 
 var angulo = 0,
     x = 300,
     y = 150;
 
-var miraComum = new Sight('Mira Comum', 'mira_comum.png');
-var miraPontoVermelho = new Sight('Ponto Vermelho', 'mira_ponto_vermelho.png');
 var balaPistola = new Image();
 balaPistola.src = './resources/armas/bala_pistola.png';
 
@@ -46,7 +88,9 @@ function drawArma (arma, CONTEXT, mouse = [ 2 ]) {
     CONTEXT.drawImage(arma.img, -arma.img.width / 2, -arma.img.height / 2);
 
     CONTEXT.restore();
-    CONTEXT.drawImage(miraPontoVermelho.img, mouse[0], mouse[1]);
+
+    //desenha mira da arma
+    CONTEXT.drawImage(arma.attachment[0].extra.img, mouse[0], mouse[1]);
 
 }
 
