1ba85b82c3f06786a72bc6bca90f8c2297a708b6
Weuller Jr. Souza <weuller9505@live.com>
2018-07-02 11:30:56 -0300

    Controles

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const ARMA_TESTE = fuzil1;
 var intervalTime = 1000 / FPS;
 var mouseX, mouseY;
 var tirosNoAr = new Array(0);
+var keyPressList = [];
 window.addEventListener('load', function () {
     canvasApp();
 });
@@ -12,12 +13,38 @@ THECANVAS.addEventListener('click', function () {
     drawTiro(balaPistola, ARMA_TESTE, tirosNoAr, [ 300, 150 ], [ mouseX, mouseY ], CONTEXT);
 });
 
+document.onkeydown = function (e) {
+    e = e ? e : window.event;
+    keyPressList[e.keyCode] = true;
+};
+
+document.onkeyup = function (e) {
+    e = e ? e : window.event;
+    keyPressList[e.keyCode] = false;
+};
+
+function checkKeys () {
+    if (keyPressList[65]) { //A
+        militar.andar(100, 100);
+    }
+    if (keyPressList[68]) { //D
+        militar.andar(100, 100);
+    }
+    if (keyPressList[83]) { //S
+        militar.andar(100, 100);
+    }
+    if (keyPressList[87]) { //W
+        militar.andar(100, 100);
+    }
+}
+
 function onMouseMove (e) {
     mouseX = e.clientX - THECANVAS.offsetLeft;
     mouseY = e.clientY - THECANVAS.offsetTop;
 }
 
 function gameLoop () {
+    checkKeys();
     drawScreen();
     devInfo(THECANVAS, CONTEXT, mouseX, mouseY);
     window.setTimeout(gameLoop, intervalTime);
@@ -42,7 +69,6 @@ function drawScreen () {
     CONTEXT.fillRect(0, 0, 600, 200);
 
     militar.spawn(100, 100);
-    militar.andar(100, 100);
     drawArma(ARMA_TESTE ,CONTEXT, [ mouseX, mouseY ]);
     updateTiro(tirosNoAr, CONTEXT);
 }
